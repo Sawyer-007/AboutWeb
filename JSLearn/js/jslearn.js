b6c1e99ec65730112112942b37404f4826fa24ab
Sawyer-007 <sawyer_007@outlook.com>
2016-03-18 17:02:07 +0800

    2016-3-18

--- JSLearn/js/jslearn.js
+++ JSLearn/js/jslearn.js
@@ -3,11 +3,44 @@
  */
 
 var oDiv1 = document.getElementById('div1'),
-    oDiv2 = document.getElementById('div2');
+    oDiv2 = document.getElementById('div2'),
+    oDiv3 = document.getElementById('div3'),
+    oDiv4 = document.getElementById('div4');
 oDiv1.onclick = function() {
-    alert('1');
+    console.log('1');
 };
 oDiv2.onclick = function(e) {
-    alert('2');
+    console.log('2');
     e.stopPropagation();
+   // e.cancelBubble = true;  //也可利用修改事件属性来阻止冒泡
+    console.log(e);
 };
+oDiv3.addEventListener('click',function() {
+    console.log('3');
+},true);
+oDiv4.addEventListener('click',function() {
+    console.log('4');
+},true);
+var oList = document.getElementById('myList');
+oList.addEventListener('click', function () {
+    console.log('5');
+}, false);
+/*
+function shout () {
+    return 222;
+}
+var Pet=function()
+{
+    this.msg="Please show me delicious food…";
+    this.shout = function()
+    {
+        console.log(this.msg);
+    }
+    this.waitAndShout = function()
+    {
+        var that = this;
+        setInterval(function(){that.shout()},2000);
+        //setInterval(this.shout.bind(this),2000);
+    }
+}
+    */
