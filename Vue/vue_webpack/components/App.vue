40c4bebf08e3bbe3d1ca3a47c57185075da70cd4
Sawyer-007 <sawyer_007@outlook.com>
2016-06-15 11:18:17 +0800

    Vue x webpack

--- Vue/vue_webpack/components/App.vue
+++ Vue/vue_webpack/components/App.vue
@@ -1,17 +1,68 @@
+<style>
+	.app {
+		position: relative;
+		margin-top: 10%;
+		margin-left: 25%;
+		width: 50%;
+		border-radius: 20px 20px 20px 20px;
+		overflow: hidden;
+	}
+	.app-header {
+		background: #d2edf4;
+		background-image: linear-gradient(to bottom, #d0edf5, #e1e5f0 100%);
+		padding: 20px 15px 15px 15px;
+		position: relative;
+	}
+	.app-header input {
+		border: none;
+		background: rgba(0,0,0,0);
+		outline: none;
+		height: 30px;
+		width: 100%;
+		font-size: 1em;
+	}
+	.app-todo {
+		position: relative;
+		background: #ffffff;
+	}
+	.app-todo ul {
+		margin: 0;
+		list-style: none;
+		padding: 0;
+	}
+	.app-todo-btn-group {
+		position: relative;
+		height: 50px;
+		line-height: 50px;
+		text-align: center;
+	}
+	.app-todo-btn {
+		display: inline-block;
+		background: #ffffff;
+		color: #ff3b2f;
+		border: 1px solid #ff3b2f;
+		border-radius: 4px;
+		outline: none;
+	}
+</style>
 <template>
-	<div id="app" >
-		<input type="text" 
-			placeholder="What's need to be done?" 
-			@keyup.enter="addTodo"
-			v-model="newTodo">
+	<div id="app" class="app">
+		<div class="app-header">
+			<h2>Todo List</h2>
+			<input type="text"
+						 placeholder="What's need to be done?"
+						 @keyup.enter="addTodo"
+						 v-model="newTodo">
+		</div>
+
 		<div class="app-todo" v-show="todoList.length">
 			<ul>
 				<item v-for="todo in filteredTodos" :attr="todo" ></item>
 			</ul>
-			<div class="app-todo-btn">
-				<button @click="changeVisibility('all')">ALL</button>
-				<button @click="changeVisibility('active')">ACTIVE</button>
-				<button @click="changeVisibility('completed')">COMPLETED</button>
+			<div class="app-todo-btn-group">
+				<button class="app-todo-btn" @click="changeVisibility('all')">ALL</button>
+				<button class="app-todo-btn" @click="changeVisibility('active')">ACTIVE</button>
+				<button class="app-todo-btn" @click="changeVisibility('completed')">COMPLETED</button>
 			</div>
 		</div>
 	</div>
--- Vue/vue_webpack/components/Item.vue
+++ Vue/vue_webpack/components/Item.vue
@@ -1,9 +1,63 @@
+<style>
+	.item {
+		position: relative;
+		height: 50px;
+		font-size: 1.125em;
+		border-bottom: 1px solid #dfdfdf;
+	}
+	.item input {
+
+	}
+	.item-remove {
+		position: absolute;
+		right: 20px;
+		background: #ffffff;
+		border: none;
+		outline: none;
+		font-size: 1.25em;
+	}
+	.item-toggle {
+		position: relative;
+		display: inline-block;
+		border: 1px solid #8e8e8e;
+		border-radius: 50%;
+		height: 24px;
+		width: 24px;
+	}
+	.item-toggle span {
+		position: absolute;
+		top: 4px;
+		left: 4px;
+		color: #5cb85c;
+	}
+	.item-view {
+		position: absolute;
+		top: 10px;
+		height: 30px;
+		width: 95%;
+		padding-left: 15px;
+	}
+	.item-text {
+		position: absolute;
+		height: 30px;
+		top: 0;
+		left: 55px;
+		line-height: 26px;
+	}
+	.item-text-finish {
+		text-decoration: line-through;
+		color: #8e8e8e;
+	}
+
+</style>
 <template>
-	<li>
-		<div class="view" v-show="!isEditing">
-			<input type="checkbox" v-model="attr.completed">
-			<label @dblclick="editTodo">{{attr.title}}</label>
-			<button @click="removeTodo" >x</button>
+	<li class="item">
+		<div class="item-view" v-show="!isEditing">
+			<div class="item-toggle" @click="finishToggle">
+				<span class="icon-ok" v-show="attr.completed"></span>
+			</div>
+			<span class="item-text" :class="{'item-text-finish': attr.completed}" @dblclick="editTodo">{{attr.title}}</span>
+			<button class="item-remove" @click="removeTodo" >x</button>
 		</div>
 		<input type="text" 
 		v-model="attr.title"
@@ -27,6 +81,9 @@
 		},
 		props:['attr'],
 		methods: {
+			finishToggle () {
+				this.attr.completed = !this.attr.completed;
+			},
 			editTodo () {
 				this.isEditing = true;
 				this.beforeEditing = this.attr.title;
